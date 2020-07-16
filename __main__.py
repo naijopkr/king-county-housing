21b1f6301108be5e8c31d1d96932b767534aa4fd
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-07-16 16:10:57 -0500

    implements eda

--- __main__.py
+++ __main__.py
@@ -1,2 +1,20 @@
-if __name__ == '__main__':
-    print('Hello world')
+import pandas as pd
+
+df = pd.read_csv('data/kc_house_data.csv')
+df.head()
+df.describe().transpose()
+df.isnull().sum()
+
+# EDA
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot(callplot: callable, *args, **kwargs):
+    plt.figure(figsize=(12, 8))
+    callplot(*args, **kwargs)
+
+
+plot(sns.distplot, df['price'])
+plot(sns.countplot, df['bedrooms'])
+plot(sns.scatterplot, 'price', 'sqft_living', data=df)
+plot(sns.boxplot, 'bedrooms', 'price', data=df)
